b748d6a6faa5f396cc36654bfc5e9f9ab20b2128
Helpfullinx <zachvdub2002@gmail.com>
2025-02-26 12:16:20 -0600

    Added homepage

--- nfina_benchmark_app/app/settings.py
+++ nfina_benchmark_app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--#v+6-_dj574^2iwph-fsj+zluxn1b%c(e1jmat63=y5-s&i-^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Add your static directory here
+]
+
+STATIC_ROOT = BASE_DIR / '.static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
